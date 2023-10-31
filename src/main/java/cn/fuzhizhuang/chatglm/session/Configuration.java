7a827879e005f31cc5c321471d9a13777125d167
zhuangzhi <71255902101@stu.ecnu.edu.cn>
2023-10-31 19:19:36 +0800

    feat:统一配置项管理

--- src/main/java/cn/fuzhizhuang/chatglm/session/Configuration.java
+++ src/main/java/cn/fuzhizhuang/chatglm/session/Configuration.java
@@ -109,8 +109,8 @@ public class Configuration {
     /**
      * http keywords
      */
-    public static final String SSE_CONTENT_TYPE="text/event-stream";
-    public static final String DEFAULT_USER_AGENT="Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)";
-    public static final String APPLICATION_JSON="application/json";
-    public static final String JSON_CONTENT_TYPE=APPLICATION_JSON + "; charset=utf-8";
+    public static final String SSE_CONTENT_TYPE = "text/event-stream";
+    public static final String DEFAULT_USER_AGENT = "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)";
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String JSON_CONTENT_TYPE = APPLICATION_JSON + "; charset=utf-8";
 }
